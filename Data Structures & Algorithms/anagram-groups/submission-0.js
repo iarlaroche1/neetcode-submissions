a9fe840d96c1e5da639cdb0ff3cc21aafb249a0d
Iarla Roche <i.roche2@universityofgalway.ie>
2026-05-18 09:40:21 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let a = 'a'.charCodeAt(0);
+        const map = new Map();
+
+        for(let i = 0; i < strs.length; i++){
+            let word = strs[i];
+            let freqArray = new Array(26).fill(0);
+            
+            for(let j = 0; j < word.length; j++){
+                let charCode = word[j].charCodeAt(0) - a;
+                freqArray[charCode]++;
+            }
+            let freqArrayString = freqArray.join();
+            if(!map.has(freqArrayString)){
+                map.set(freqArrayString, [word]);
+            }
+            else {
+                map.get(freqArrayString).push(word);
+            }
+
+        }
+        let grouped = [];
+        for(const [key, val] of map){
+            grouped.push(val);
+     
+        }
+        return grouped;
+    }
+}
